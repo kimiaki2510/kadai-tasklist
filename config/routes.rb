35f76ed58e5dfcff92cfa6e50cb4f974cdb861b1
kimiaki.sugiyama <kimiaki.sugiyama@techacademy.jp>
2019-08-31 02:14:26 +0000

    add bootstrap design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # URLが xxxx.com/ でアクセスされたときにtasksコントローラのindexメソッドに処理を渡す
   root to: 'tasks#index'
 
+  # resourcesはよくあるルーティングのセットをひとまとめに定義できるメソッド
+  # tasksモデルに関する一連のルーティングを定義している
   resources :tasks
 end
