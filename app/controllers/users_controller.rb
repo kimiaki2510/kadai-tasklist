539f0b462ee88ca59c6f27eef453de55264524e9
kimiaki.sugiyama <kimiaki.sugiyama@techacademy.jp>
2019-08-31 06:50:38 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -19,6 +20,7 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   #URLが xxxx.com/ でアクセスされたときにusersコントローラのindexメソッドに処理を渡す
   root to: 'users#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  #最初の画面をログイン画面に設定
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   
