38c5655aa8e6f3281b3ae625f91103487468769d
kimiaki.sugiyama <kimiaki.sugiyama@techacademy.jp>
2019-09-01 06:08:33 +0000

    kadai-tasklist

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
+    #Userとtaskの１対多を表現
+    belongs_to :user
+    #内容の空欄なし10字以内
     validates :content, presence: true, length: {maximum: 10}
+    #タスクの空欄なし10字以内
     validates :status, presence: true, length: {maximum: 10}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
     has_secure_password
+    
+    #userからtaskを見た時複数存在する
+    has_many :tasks
 end
