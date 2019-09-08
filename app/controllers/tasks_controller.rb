c0fc77b26edd30ba4970d364c1374156f00b49b6
kimiaki.sugiyama <kimiaki.sugiyama@techacademy.jp>
2019-09-08 10:50:26 +0000

    heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,12 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
-    before_action :ensure_correct_user, only: [:show, :edit, :update, :destroy]
     def index
+        #@tasks = current_user.tasks.page(params[:page]).per(3)
         @tasks = Task.order(id: :desc).page(params[:page]).per(25)
-        #@tasks = current_user.tasks.page(params[:page]).per(25)
-        #@tasks = Task.order(id: :desc).page(params[:page]).per(3)
-        #@tasks = Task.find_by(id: params[:id])
-        #@user = User.find_by(id: @task.user_id)
     end
     
     def new
-        #@task = Task.new
-        @task = Task.new(
-        status: params[:status],
-        content: params[:content],
-        user_id: @current_user.id
-            )
+        @task = Task.new
     end
     
     def create
@@ -30,11 +21,11 @@ class TasksController < ApplicationController
     end
     
     def edit
-        ensure_correct_user
+        set_task
     end
     
     def update
-        ensure_correct_user
+        set_task
         if @task.update(task_params)
             flash[:success] = 'Taskは正常に更新されました'
             redirect_to @task
@@ -45,10 +36,10 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        ensure_correct_user
+        set_task
         @task.destroy
         flash[:success] = 'Taskは正常に削除されました'
-        render root_url
+        redirect_to tasks_url
     end
     
     def show
@@ -57,22 +48,13 @@ class TasksController < ApplicationController
     
     private
     
-    def ensure_correct_user
-        set_task
-        if @task.user_id != @current_user.id
-            flash[:notice] = '権限がありません'
-            redirect_to root_url
-        end
-    end
-    
     def set_task
-        #@task = Task.find(params[:id])
-        #@task = Task.find(params[:id])
-        @task = Task.find_by(id: params[:id])
+        @task = Task.find(params[:id])
     end
     
     def task_params
         params.require(:task).permit(:content, :status)
+        
     end
     
 end
