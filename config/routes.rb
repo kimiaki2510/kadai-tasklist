909658295b32870c54f89562b08f28945d7e8723
kimiaki.sugiyama <kimiaki.sugiyama@techacademy.jp>
2019-09-18 13:19:43 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,11 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :users, only: [:index, :show, :new, :create]
+  #resources :users, only: [:index, :show, :new, :create]
   
   # URLが xxxx.com/ でアクセスされたときにtasksコントローラのindexメソッドに処理を渡す
   root to: 'tasks#index'
   get 'signup', to: 'users#new'
-  
   # resourcesはよくあるルーティングのセットをひとまとめに定義できるメソッド
   # tasksモデルに関する一連のルーティングを定義している
   resources :tasks
