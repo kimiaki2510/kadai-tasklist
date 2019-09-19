4a18bea0e1373b702bc765ce452b2d32e6ba833b
kimiaki.sugiyama <kimiaki.sugiyama@techacademy.jp>
2019-09-19 08:55:30 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   #resources :users, only: [:index, :show, :new, :create]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :index]
   
   # URLが xxxx.com/ でアクセスされたときにtasksコントローラのindexメソッドに処理を渡す
   root to: 'tasks#index'
